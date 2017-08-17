2891d6a92c8dba31d0f9a5ffe26604069f1f2e09
Julio Montas <hello@juliomontas.com>
2017-08-17 19:35:49 -0400

    updates, still not finish

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -38,6 +38,10 @@
  */
 // =require vendor/tabbed.js
 
+/*!
+ * lightbox.js
+ */
+// =require vendor/lightbox.js
 
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
