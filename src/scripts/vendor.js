a97dfb8284efdb3f173c2caa21fc91d2c86ca994
Julio Montas <hello@juliomontas.com>
2017-08-21 13:30:34 -0400

    lighbox fix

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -43,6 +43,8 @@
  */
 // =require vendor/lightbox.js
 
+
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
