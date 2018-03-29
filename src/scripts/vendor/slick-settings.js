de059ca3336aa01f628987481e7a57b73178d6db
JulioMontas <hello@juliomontas.com>
2018-03-29 17:51:37 -0400

    video format update

--- src/scripts/vendor/slick-settings.js
+++ src/scripts/vendor/slick-settings.js
@@ -75,10 +75,36 @@ $('.product-slides').slick({
   nextArrow:"<button type='button' class='slick-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
 });
 
+// 
 // $('.productCircleButtons-slider').slick({
 //   speed: 300,
 //   infinite: false,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   arrows: false,
+//   slidesToShow: 5,
+//   slidesToScroll: 5,
+//   arrows: true,
+//   prevArrow:"<button type='button' class='slick-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//   nextArrow:"<button type='button' class='slick-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
 // });
