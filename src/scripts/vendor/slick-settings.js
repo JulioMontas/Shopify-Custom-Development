32c90dba2cd46d27cebee6406d84bf6560900955
JulioMontas <hello@juliomontas.com>
2018-08-08 00:09:54 -0400

    remove some collections

--- src/scripts/vendor/slick-settings.js
+++ src/scripts/vendor/slick-settings.js
@@ -32,15 +32,15 @@ $('.list-of-five').slick({
   ]
 });
 
-
 $('.list-of-four').slick({
   speed: 300,
   infinite: false,
   slidesToShow: 4,
   slidesToScroll: 4,
+  autoplay:true,
   arrows: true,
-  prevArrow:"<button type='button' class='slick-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-  nextArrow:"<button type='button' class='slick-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  prevArrow:"<button type='button' class='slick-prev'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
   responsive: [
     {
       breakpoint: 1024,
@@ -54,6 +54,108 @@ $('.list-of-four').slick({
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.list-of-three').slick({
+  speed: 300,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay:true,
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.list-of-two').slick({
+  speed: 300,
+  infinite: false,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay:true,
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.list-of-one').slick({
+  speed: 300,
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay:true,
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
